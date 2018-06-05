882f92328fa8db2907954e203a13d7dbeb46406f
Jean-Christophe Meillaud <jc@houseofagile.com>
2018-06-05 14:56:39 +0200

    Update Readme with def of ENV variables

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # docker-ubuntu-ssh
-docker container based on ubuntu with ssh access
+
+Minimal docker container based on ubuntu (phusion-image) with sudoless deployment user and ssh access, as well as root access.
+
+Works with ENV variables:
+
+    # Deploy user login, default to 'ituser'
+    SSH_USER=${SSH_USER:-ituser}
+
+    # Deploy user password, default to login
+    SSH_PASSWORD=${SSH_PASSWORD:-$SSH_USER}
+
+    # contains string of public ssh key
+    SSH_AUTHORIZED_KEYS=${SSH_AUTHORIZED_KEYS:-ituser}
+
+
+## How to run a container
+    docker run -d --env 'SSH_USER=jmeyo' --env 'SSH_PASSWORD=jmeyo' --env "SSH_AUTHORIZED_KEYS=$(cat ~/.ssh/id_rsa.pub)" jmeyo/docker-ubuntu-ssh-my
--- init_users.sh
+++ init_users.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 SSH_USER=${SSH_USER:-ituser}
 SSH_PASSWORD=${SSH_PASSWORD:-$SSH_USER}
 SSH_AUTHORIZED_KEYS=${SSH_AUTHORIZED_KEYS}
@@ -11,9 +10,14 @@ echo "$SSH_USER:$SSH_PASSWORD" | chpasswd && \
 mkdir -p /home/$SSH_USER/.ssh/ /root/.ssh/ && \
 ssh-keygen -A && \
 adduser $SSH_USER sudo && \
-adduser $SSH_USER www-data && \
+adduser $SSH_USER www-data
+
 if [ "${SSH_AUTHORIZED_KEYS}x" != "x" ]; then
   echo $SSH_AUTHORIZED_KEYS > /home/$SSH_USER/.ssh/authorized_keys && \
   echo $SSH_AUTHORIZED_KEYS > /root/.ssh/authorized_keys
 fi
+
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL">/etc/sudoers.d/90-allow-sudoless-for-$SSH_USER
+sed -i.bkp -e \
+      's/%sudo\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%sudo ALL=NOPASSWD:ALL/g' \
+      /etc/sudoers
