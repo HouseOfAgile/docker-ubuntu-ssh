4b99b177744bda9065126a76c7d4dc2f6a91ae4d
Jean-Christophe Meillaud <jc@houseofagile.com>
2018-06-05 13:25:04 +0200

    Add authorized_keys only if defined

--- init_users.sh
+++ init_users.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+set -x
 SSH_USER=${SSH_USER:-ituser}
 SSH_PASSWORD=${SSH_PASSWORD:-$SSH_USER}
-SSH_AUTHORIZED_KEYS=${SSH_AUTHORIZED_KEYS:-ituser}
+SSH_AUTHORIZED_KEYS=${SSH_AUTHORIZED_KEYS}
 
 useradd -ms /bin/bash $SSH_USER && \
 # dummy password as login
@@ -12,6 +12,8 @@ mkdir -p /home/$SSH_USER/.ssh/ /root/.ssh/ && \
 ssh-keygen -A && \
 adduser $SSH_USER sudo && \
 adduser $SSH_USER www-data && \
-echo $SSH_AUTHORIZED_KEYS > /home/$SSH_USER/.ssh/authorized_keys && \
-echo $SSH_AUTHORIZED_KEYS > /root/.ssh/authorized_keys
+if [ "${SSH_AUTHORIZED_KEYS}x" != "x" ]; then
+  echo $SSH_AUTHORIZED_KEYS > /home/$SSH_USER/.ssh/authorized_keys && \
+  echo $SSH_AUTHORIZED_KEYS > /root/.ssh/authorized_keys
+fi
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL">/etc/sudoers.d/90-allow-sudoless-for-$SSH_USER
